706dc619d62bcc5907516448d45e3cc8f2bd7e10
Akash <akjaisawal05@gamail.com>
2024-03-02 13:55:10 +0530

    otp page added

--- app/src/main/java/com/akash/hostyourcar/MainActivity.java
+++ app/src/main/java/com/akash/hostyourcar/MainActivity.java
@@ -2,13 +2,27 @@ package com.akash.hostyourcar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OtpVerify.class));
+            }
+        });
     }
 }
